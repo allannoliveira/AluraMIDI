5d23a8815fb1c1290ffe84377c44b8e2afbdc07c
Allan Oliveira <allansilvaoliveira@outlook.com>
2022-09-04 15:23:42 -0300

    add js

--- js/main.js
+++ js/main.js
@@ -5,20 +5,31 @@ function TocaSom(idElementAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {  
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {  
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
 
     const idAudio = `#som_${instrumento}`;
 
-    listaDeTeclas[contador].onclick = function (){
+    tecla.onclick = function (){
         TocaSom(idAudio);
     };
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+    
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
+    
 }
 
     
